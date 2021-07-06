918ece1cabf8dac056899834e96061b3536881ae
Artoiss <aarttu@windowslive.com>
2021-07-06 19:53:20 +0300

    Golang hash

---
+++ Part1/Exercise_1.01/timestamper-go/stamp.go
@@ -0,0 +1,33 @@
+package main
+
+import ("fmt"
+        "time"
+        "math/rand"
+        "strings"
+)
+
+func main(){
+  // Generate hash
+  var hash = randomStr(64)
+
+  // Save hash
+
+  // Print timestamp and hash
+  for range time.Tick(time.Second * 5) {
+    currentTime := time.Now()
+    fmt.Println(currentTime.Format("2006-01-02T01:04:05.705Z") + " : " + hash)
+  }
+}
+
+// Source code here: https://yourbasic.org/golang/generate-random-string/
+func randomStr(length int) string {
+  rand.Seed(time.Now().UnixNano())
+  chars := []rune("!#€%&/()=?$|[]abcdefghijklmnopqrstuvwxyzåäöABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ" +
+    "abcdefghijklmnopqrstuvwxyzåäö0123456789")
+  var b strings.Builder
+  for i := 0; i < length; i++ {
+    b.WriteRune(chars[rand.Intn(len(chars))])
+  }
+  str := b.String()
+	return string(str)
+}
